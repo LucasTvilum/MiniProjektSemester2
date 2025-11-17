73aff514c7bbe0cb84d9ccd7262f79a82f24e1b5
selale920 <selaleg8@gmail.com>
2025-11-17 11:11:16 +0100

    repository

---
+++ ServerApp/Repository/IAnnonceRepository.cs
@@ -0,0 +1,14 @@
+namespace ServerApp.Repository;
+using Core.Models;
+
+public interface IAnnonceRepository
+{
+    List<Annonce> GetAll();
+    
+    Annonce Add(Annonce annonce);
+    
+    Annonce Update(Annonce annonce);
+    
+    void Delete(Annonce annonce);
+    
+}
