e445957f56d3994a527056f777db20e08466572a
LucasTvilum <lucastvilum@gmail.com>
2025-11-17 11:18:05 +0100

    bla

--- ServerApp/Models/Annonce.cs
+++ ServerApp/Models/Annonce.cs
@@ -8,7 +8,6 @@ namespace ServerApp.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
         public string Description { get; set; }
         public double Price { get; set; }
         public string Type { get; set; }
