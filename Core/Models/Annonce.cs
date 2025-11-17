a56274be47562ada56c09c41c059b7ef4fba4ff7
1sra0mar <israaaomar22@gmail.com>
2025-11-17 09:54:32 +0100

    oprettet annonce.cs og annonce.razor

---
+++ Core/Models/Annonce.cs
@@ -0,0 +1,10 @@
+namespace Core.Models;
+
+public class Annonce
+{
+    public int Id { get; set; }
+    public string Titel { get; set; }
+    public int Pris { get; set; }
+    public string BilledeUrl { get; set; }
+    
+}
