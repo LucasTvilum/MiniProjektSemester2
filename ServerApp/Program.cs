03764c13e4794993ab9c3414dd401575a20b9d55
selale920 <selaleg8@gmail.com>
2025-11-17 12:47:00 +0100

    program update

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,14 +1,41 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
+using ServerApp.Repository;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IAnnonceRepository, AnnonceMongoDB>();
+builder.Services.AddSingleton<IBrugerRepository, BrugerMongoDB>();
+builder.Services.AddSingleton<ILokaleRepository, LokaleMongoDB>();
+
+builder.Services.AddCors(options =>
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello from Meaamdddaddmamdjdsddssddjdjjddin");
-        // Your startup code hered
-    }
+    options.AddPolicy("AllowBlazor",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
 }
-//bkabaass
+
+app.UseHttpsRedirection();
+
+app.UseCors("policy");
+
+app.UseAuthorization();
+
+app.UseCors("AllowBlazor");
+
+app.MapControllers();
+
+app.Run();
