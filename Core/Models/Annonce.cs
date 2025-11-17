77505fd832eb9268bff8cb3425bb6958a85bcc37
1sra0mar <israaaomar22@gmail.com>
2025-11-17 11:36:41 +0100

    Tøjets aktiv status

--- Core/Models/Annonce.cs
+++ Core/Models/Annonce.cs
@@ -6,7 +6,8 @@ public class Annonce
     public string Titel { get; set; }
     public int Pris { get; set; }
     public string BilledeUrl { get; set; }
-    
     public string type { get; set; }
+    
+    public bool Aktiv { get; set; }
     
 }
